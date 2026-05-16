ac5b741eb2989e4bb9c6416c8aee56c023c508dd
codexuz <examonlineedumo@gmail.com>
2026-05-17 00:41:35 +0500

    module added

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportExportService } from './report-export.service';
 import { ReportsController } from './reports.controller';
+import { ExchangeRatesModule } from '../exchange-rates/exchange-rates.module';
 
 @Module({
+  imports: [ExchangeRatesModule],
   controllers: [ReportsController],
   providers: [ReportsService, ReportExportService],
   exports: [ReportsService],
